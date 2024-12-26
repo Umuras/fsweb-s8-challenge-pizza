08de56a8cca46699637f164f2893e96bd4262454
Ali Umur Kucur <aliumurkucur@gmail.com>
2024-12-26 11:06:12 +0300

    Footer and Success page made responsive form.

--- src/components/CommonFooter.jsx
+++ src/components/CommonFooter.jsx
@@ -13,33 +13,65 @@ import {
   CardText,
 } from "reactstrap";
 
+const Footer = styled.footer`
+  background-color: black;
+
+  @media screen and (max-width: 765px) {
+    height: 158vh;
+  }
+`;
+
+const SubFooter = styled.div``;
+
+const AllInfoAreaContainer = styled.section`
+  width: 69.5%;
+  margin-left: 20rem;
+  height: 20rem;
+  display: flex;
+  justify-content: space-between;
+
+  @media screen and (max-width: 765px) {
+    margin-top: 4rem;
+    flex-direction: column;
+    margin-left: 16rem;
+  }
+`;
+
+const InfoMenuContainer = styled.div`
+  display: flex;
+  width: 50%;
+  height: 22rem;
+`;
+
 const TechnologicFoodsContainer = styled.div`
   color: white;
   display: flex;
-
-  width: 50%;
+  width: 25%;
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
   row-gap: 2rem;
+  margin-top: 3rem;
+
+  @media screen and (max-width: 765px) {
+    margin-top: 3rem;
+    width: 60%;
+  }
 `;
 
 const ContactContainer = styled.div`
   display: flex;
   flex-direction: column;
-  row-gap: 1rem;
+  row-gap: 1.5rem;
+  margin-top: 1rem;
 `;
 
 const ContactImg = styled.img`
   margin-right: 1rem;
 `;
 
-const InfoMenuContainer = styled.div`
-  display: flex;
-
-  width: 50%;
-  height: 22rem;
-  margin-top: 5rem;
+const MenuInfoContainer = styled.div`
+  color: white;
 `;
 
 const MenuTitleLabel = styled.p`
@@ -48,10 +80,7 @@ const MenuTitleLabel = styled.p`
   font-weight: bold;
   font-family: "Barlow", serif;
   font-size: 1.25rem;
-`;
-
-const MenuInfoContainer = styled.div`
-  color: white;
+  margin-top: 7rem;
 `;
 
 const MenusAreaContainer = styled.div`
@@ -62,26 +91,15 @@ const MenusAreaContainer = styled.div`
   row-gap: 0.8rem;
 `;
 
-const AllInfoAreaContainer = styled.section`
-  width: 69.5%;
-  margin-left: 20rem;
-  height: 20rem;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const BorderLine = styled.p`
-  margin-top: 10rem;
-  border: 1px solid gray;
-`;
-
 const InstaContainer = styled.div`
-  width: 25.47%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  margin-top: 6.75rem;
   height: 32vh;
+  margin-top: 6.75rem;
+  @media screen and (max-width: 765px) {
+    margin-top: 3rem;
+  }
 `;
 
 const InstaLabel = styled.p`
@@ -102,6 +120,20 @@ const InstaImgContainer = styled.div`
   justify-content: center;
   gap: 0.5rem;
   align-items: flex-end;
+  width: 60%;
+
+  @media screen and (max-width: 765px) {
+    width: 60%;
+  }
+`;
+
+const BorderLine = styled.p`
+  margin-top: 10rem;
+  border: 1px solid gray;
+
+  @media screen and (max-width: 765px) {
+    margin-top: 60rem;
+  }
 `;
 
 const SubLabel = styled(Label)`
@@ -113,38 +145,49 @@ const FontIconLabel = styled(Label)`
   margin-left: 0.35rem;
 `;
 
+const LowestArea = styled.section`
+  display: flex;
+  justify-content: space-between;
+  width: 70%;
+  margin-left: 20rem;
+  align-items: center;
+
+  @media screen and (max-width: 765px) {
+    margin-left: 16rem;
+    width: 58%;
+  }
+`;
+
 export default function CommonFooter() {
   return (
-    <footer className="restaurantinfocontainer">
-      <div>
+    <Footer>
+      <SubFooter>
         <AllInfoAreaContainer>
-          <InfoMenuContainer>
-            <TechnologicFoodsContainer>
-              <img src="../../Assets/Iteration-2-aseets/footer/logo-footer.svg" />
-              <ContactContainer>
-                {helpInfos.map((item, index) => {
-                  return (
-                    <div>
-                      <ContactImg src={item.icon} />
-                      <Label>{item.text}</Label>
-                      {index === 0 && <SubLabel>{item.text2}</SubLabel>}
-                    </div>
-                  );
-                })}
-              </ContactContainer>
-            </TechnologicFoodsContainer>
-            <MenuInfoContainer>
-              <MenuTitleLabel>Sıccacık Menüler</MenuTitleLabel>
-              <MenusAreaContainer>
-                <Label>Terminal Pizza</Label>
-                <Label>5 Kişilik Hackathlon Pizza</Label>
-                <Label>useEffect Tavuklu Pizza</Label>
-                <Label>Beyaz Console Frost</Label>
-                <Label>Testler Geçti Mutlu Burger</Label>
-                <Label>Position Absolute Acı Burger</Label>
-              </MenusAreaContainer>
-            </MenuInfoContainer>
-          </InfoMenuContainer>
+          <TechnologicFoodsContainer>
+            <img src="../../Assets/Iteration-2-aseets/footer/logo-footer.svg" />
+            <ContactContainer>
+              {helpInfos.map((item, index) => {
+                return (
+                  <div>
+                    <ContactImg src={item.icon} />
+                    <Label>{item.text}</Label>
+                    {index === 0 && <SubLabel>{item.text2}</SubLabel>}
+                  </div>
+                );
+              })}
+            </ContactContainer>
+          </TechnologicFoodsContainer>
+          <MenuInfoContainer>
+            <MenuTitleLabel>Sıccacık Menüler</MenuTitleLabel>
+            <MenusAreaContainer>
+              <Label>Terminal Pizza</Label>
+              <Label>5 Kişilik Hackathlon Pizza</Label>
+              <Label>useEffect Tavuklu Pizza</Label>
+              <Label>Beyaz Console Frost</Label>
+              <Label>Testler Geçti Mutlu Burger</Label>
+              <Label>Position Absolute Acı Burger</Label>
+            </MenusAreaContainer>
+          </MenuInfoContainer>
 
           <InstaContainer>
             <InstaLabel>Instagram</InstaLabel>
@@ -155,17 +198,17 @@ export default function CommonFooter() {
             </InstaImgContainer>
           </InstaContainer>
         </AllInfoAreaContainer>
-      </div>
+      </SubFooter>
 
       <BorderLine />
 
-      <section className="lowestareacontainer">
+      <LowestArea>
         <div>
           <FontAwesomeIcon icon={faCircleUser} className="fonticon" />
           <FontIconLabel>2023 Teknolojik Yemekler.</FontIconLabel>
         </div>
         <FontAwesomeIcon icon={faTwitter} className="fa-2x fonticon" />
-      </section>
-    </footer>
+      </LowestArea>
+    </Footer>
   );
 }
